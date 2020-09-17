55ae58a01073836ccc1f1f721239affe42211a70
takuya-ohno <ohno.takuya.1502@gmail.com>
2020-09-17 13:15:34 +0900

    千恋＊万花サイト作成開始

---
+++ GameTitle/千恋＊万花/toppage.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<head>
+  <!-- 文字コード-->
+  <meta charset="utf-8">
+
+  <!-- SEO-->
+  <meta name="description" content="HelltakerのChapter1攻略"/>
+  <meta name="keywords" content="Helltaker, STEAM">
+
+  <!-- ライブラリ -->
+  <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+  <!-- 独自css-->
+  <link rel="stylesheet" type="text/css" href="../../css/common.css">
+  <link rel="stylesheet" type="text/css" href="../../css/header.css">
+  <link rel="stylesheet" type="text/css" href="css/header-background.css">
+
+  <title>Helltaker攻略 : Enigma攻略サイト</title>
+</head>
+<body>
+
+  <!-- ヘッダー -->
+  <?php include('./Header.html'); ?>
+
+
+  <!-- コンテンツ-->
+  <div class="container-fluid">
+    <div class="row">
+      <!-- サイド コンテンツ -->
+      <div class="col-lg-2"></div>
+
+      <!-- メイン コンテンツ-->
+      <div class="col-lg-8">
+
+        <p>Steam用ソフト「Helltaker」の攻略サイトです。</p>
+
+        <!-- ゲーム情報 -->
+        <h4 class="content dropdown-bar" data-toggle="collapse" data-target="#collapse-game-info" aria-controls="collapse-game-info">ゲーム情報　▼</h4>
+
+        <table class="table-responsive collapse show" id="collapse-game-info">
+          <tr>
+            <td rowspan="4">
+              <img src="Images\icon.jpg" alt="icon.jpg" width="150" height="150">
+            </td>
+            <td class="table-primary">タイトル</td>
+            <td class="cell-info">Helltaker</td>
+          </tr>
+          <tr>
+            <td class="table-primary">機種</td>
+            <td class="cell-info">Steam</td>
+          </tr>
+          <tr>
+            <td class="table-primary">ジャンル</td>
+            <td class="cell-info">パズル</td>
+          </tr>
+          <tr>
+            <td class="table-primary">公式サイト</td>
+            <td class="cell-info"><a href="https://store.steampowered.com/app/1289310/Helltaker/" rel="nofollow">https://store.steampowered.com/app/1289310/Helltaker/</a></td>
+          </tr>
+        </table>
+
+        <!-- 公式Twitter -->
+        <h4 class="content dropdown-bar" data-toggle="collapse" data-target="#collapse-official-info" aria-controls="collapse-official-info">公式Twitter　▼</h4>
+        <nav class="table-responsive collapse show" id="collapse-official-info">
+          <a class="twitter-timeline" data-lang="ja"  data-height="800" data-theme="light" href="https://twitter.com/vanripperart?ref_src=twsrc%5Etfw">
+            Tweets by vanripperart
+          </a>
+          <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        </nav>
+      </div>
+
+      <!-- サイド コンテンツ -->
+      <div class="col-lg-2"></div>
+    </div>
+  </div>
+
+</body>
